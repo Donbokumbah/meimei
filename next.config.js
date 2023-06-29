b1fd94b4d5026a8a336163933d628a3689352d51
Donbokumbah <donbokumbah60@gmail.com>
2023-06-29 12:18:03 +0530

    just try to resolve the Module not found and try to see if it work

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-}
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;
